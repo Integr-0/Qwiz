9669d9b3ef3d69bce62126a7fac3093befe5e718
Integr-0 <erik.reitbauer@outlook.de>
2024-09-28 17:20:03 +0200

    Copyright

--- src/main/kotlin/live/qwiz/Application.kt
+++ src/main/kotlin/live/qwiz/Application.kt
@@ -1,15 +1,24 @@
+/*
+ * Copyright © 2024 Integr
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package live.qwiz
 
 import io.ktor.http.*
-import io.ktor.serialization.*
 import io.ktor.serialization.kotlinx.*
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
-import io.ktor.server.config.*
 import io.ktor.server.engine.*
-import io.ktor.server.metrics.dropwizard.*
 import io.ktor.server.netty.*
-import io.ktor.server.plugins.*
 import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.plugins.cors.routing.*
 import io.ktor.server.plugins.statuspages.*
@@ -17,19 +26,14 @@ import io.ktor.server.response.*
 import io.ktor.server.sessions.*
 import io.ktor.server.websocket.*
 import io.ktor.util.*
-import kotlinx.serialization.ExperimentalSerializationApi
-import kotlinx.serialization.KSerializer
-import kotlinx.serialization.Serializer
 import kotlinx.serialization.encodeToString
-import kotlinx.serialization.encoding.Decoder
-import kotlinx.serialization.encoding.Encoder
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonElement
 import kotlinx.serialization.json.decodeFromJsonElement
 import kotlinx.serialization.json.encodeToJsonElement
 import live.qwiz.config.ConfigSystem
 import live.qwiz.database.configureDatabases
-import live.qwiz.routing.*
+import live.qwiz.routing.handleRouting
 import live.qwiz.session.account.AccountSession
 import java.time.Duration
 
--- src/main/kotlin/live/qwiz/database/quiz/part/QuizPart.kt
+++ src/main/kotlin/live/qwiz/database/quiz/part/QuizPart.kt
@@ -1,10 +1,20 @@
+/*
+ * Copyright © 2024 Integr
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package live.qwiz.database.quiz.part
 
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.JsonElement
-import live.qwiz.database.quiz.part.select.Select
-import live.qwiz.database.quiz.part.select.option.Option
-import live.qwiz.json
 import live.qwiz.jsonTree
 import live.qwiz.playmode.socket.gamepacket.s2c.base.GameS2CPacket
 import live.qwiz.playmode.socket.gamepacket.s2c.base.S2CActions
